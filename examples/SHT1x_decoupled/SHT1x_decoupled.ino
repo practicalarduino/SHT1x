bb8b892f68f1c0bb7f5a5fe18cf886a67883f20d
lthiery <me@louisthiery.com>
2013-06-11 18:29:01 -0400

    minor clean-ups

--- examples/SHT1x_decoupled/SHT1x_decoupled.ino
+++ examples/SHT1x_decoupled/SHT1x_decoupled.ino
@@ -1,12 +1,12 @@
 #include <SHT1x.h>
 
 // Specify data and clock connections and instantiate SHT1x object
-#define dataPin  10
-#define clockPin 11
+#define DATA  2
+#define CLOCK 3
 // voltage is optional, defaults to 5
 #define VOLTAGE 3.3
 
-SHT1x sht0(dataPin, clockPin, VOLTAGE);
+SHT1x sht0(DATA, CLOCK, VOLTAGE);
 
 void setup(){
   Serial.begin(57600);
@@ -22,6 +22,8 @@ void loop(){
   temp0 = sht0.parseTemperatureC(sht0.readInTemperature());
  
   sht0.requestHumidity();
+  //it is important to parse humidity AFTER reading in temperature
+  //since parsing formula adjusts using temperature
   hum0 = sht0.parseHumidity(sht0.readInHumidity());
   
   Serial.print("SENSOR #0: ");
